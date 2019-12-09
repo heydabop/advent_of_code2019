973bfd6d18a5006a8a49f37cf52afd24c74859ff
heydabop <heydabop@gmail.com>
2019-12-09 09:42:43 -0600

    day 3 part 2

--- day03/part2/main.rs
+++ day03/part2/main.rs
@@ -44,78 +44,87 @@ fn main() {
 
     println!("{} {} {} {}", max_x, min_x, max_y, min_y);
 
-    // Use the min and max as bounds for our 2d vector
-    let mut grid = vec![vec![0; (max_x - min_x + 1) as usize]; (max_y - min_y + 1) as usize];
+    // Use the min and max as bounds for our 2d vector of pairs (so really 3d)
+    let mut grid = vec![vec![[0, 0]; (max_x - min_x + 1) as usize]; (max_y - min_y + 1) as usize];
 
     // Determine where in vector center port is
     let center_x = (0 - min_x) as usize;
     let center_y = (0 - min_y) as usize;
 
-    let mut wire = 1;
+    let mut wire = 0;
     for ref line in input.lines() {
         // Record each wire in vector, starting at center port
         let mut x = center_x;
         let mut y = center_y;
+        let mut steps = 0;
+
         for ref seg in line.split(',') {
             match parse_direction(seg) {
-                // A grid space will be either 0, 1, 2, or 3
-                // 0 - no wires
-                // 1 - first wire
-                // 2 - second wire
-                // 3 - both wires
+                // Each grid sapce has two values, each value is the number of steps a wire took to get to this space, 0 means a wire isn't on that space
                 Direction::Left(dist) => {
                     for i in 0..dist {
-                        // Travel distance to the right
-                        if grid[y][x - i] < wire {
-                            // If grid space is less than our wire value we haven't visited it as this wire yet, increment value
-                            grid[y][x - i] += wire;
+                        // Travel distance to the left
+                        if grid[y][x - i][wire] == 0 {
+                            // If the wire hasn't already been on the grid space, record how many steps it took to get here
+                            grid[y][x - i][wire] = steps;
                         }
+                        steps += 1;
                     }
                     x -= dist;
                 }
                 Direction::Right(dist) => {
                     for i in 0..dist {
-                        if grid[y][x + i] < wire {
-                            grid[y][x + i] += wire;
+                        if grid[y][x + i][wire] == 0 {
+                            grid[y][x + i][wire] = steps;
                         }
+                        steps += 1;
                     }
                     x += dist;
                 }
                 Direction::Up(dist) => {
                     for i in 0..dist {
-                        if grid[y + i][x] < wire {
-                            grid[y + i][x] += wire;
+                        if grid[y + i][x][wire] == 0 {
+                            grid[y + i][x][wire] = steps;
                         }
+                        steps += 1;
                     }
                     y += dist;
                 }
                 Direction::Down(dist) => {
                     for i in 0..dist {
-                        if grid[y - i][x] < wire {
-                            grid[y - i][x] += wire;
+                        if grid[y - i][x][wire] == 0 {
+                            grid[y - i][x][wire] = steps;
                         }
+                        steps += 1;
                     }
                     y -= dist;
                 }
             };
         }
+
+        if grid[y][x][wire] == 0 {
+            grid[y][x][wire] = steps;
+        }
         wire += 1;
     }
 
-    let mut min_dist = usize::max_value();
+    let mut min_steps = usize::max_value();
 
     // Traverse entire grid (we could start and the center and go out but this is fast enough)
     for i in 0..grid.len() {
         for j in 0..grid[0].len() {
-            // Calculate distances for grid spaces that had both wires on them and aren't the center, record the minimum
-            if (i != center_y || j != center_x) && grid[i][j] == 3 {
-                let dist = (center_y as i64 - i as i64).abs() + (center_x as i64 - j as i64).abs();
-                min_dist = min_dist.min(dist as usize);
+            let w1 = grid[i][j][0];
+            let w2 = grid[i][j][1];
+            // Check that both wires are on this grid space
+            if w1 > 0 && w2 > 0 {
+                // Record the number of steps each wire took to get here, remember the minimum
+                let steps = w1 + w2;
+                min_steps = min_steps.min(steps);
             }
         }
     }
 
-    println!("{}", min_dist);
+    println!("{}", min_steps);
 }
 
 fn parse_direction(s: &str) -> Direction {
